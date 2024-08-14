5c2daeaf6655eced29a775be8eaa58bf49fbabec
medinastr <pedromstracieri@gmail.com>
2024-08-13 23:23:19 -0300

    begin - entities created

---
+++ instructor/src/main/java/com/medinastr/instructor/InstructorApplication.java
@@ -0,0 +1,13 @@
+package com.medinastr.instructor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstructorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstructorApplication.class, args);
+	}
+
+}
---
+++ instructor/src/test/java/com/medinastr/instructor/InstructorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinastr.instructor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstructorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
